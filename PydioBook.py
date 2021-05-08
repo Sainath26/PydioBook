401c35954362eda506b8dc24a6a57f27981edf3c
Sainath26 <70280910+Sainath26@users.noreply.github.com>
2021-05-08 05:07:23 -0700

    Update PydioBook.py

--- PydioBook.py
+++ PydioBook.py
@@ -1,7 +1,10 @@
 import pyttsx3
 import pyperclip
 
+
+
 audio = pyttsx3.init()
-audio.setProperty('rate',125)
-audio.say(pyperclip.paste())
+audio.setProperty('rate',170)
+
+audio.save_to_file(pyperclip.paste(),'txct.mp3')
 audio.runAndWait()
